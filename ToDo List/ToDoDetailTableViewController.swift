77b9b847485581f2dcc5fd9e776ee12c84b7e4e2
crea2 <reacc@bc.edu>
2021-09-27 07:23:44 -0400

    5.5 complete

--- ToDo List/ToDoDetailTableViewController.swift
+++ ToDo List/ToDoDetailTableViewController.swift
@@ -9,13 +9,24 @@ import UIKit
 
 class ToDoDetailTableViewController: UITableViewController {
     @IBOutlet weak var saveBarButton: UIBarButtonItem!
-    @IBOutlet weak var nameField: UIView!
+    @IBOutlet weak var nameField: UITextField!
     @IBOutlet weak var datePicker: UIDatePicker!
     @IBOutlet weak var noteView: UITextView!
     
+    var toDoItem: String!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if toDoItem == nil {
+            toDoItem = ""
+        }
+        nameField.text = toDoItem
 
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        toDoItem = nameField.text
     }
 
     @IBAction func cancelButtonPressed(_ sender: Any) {
