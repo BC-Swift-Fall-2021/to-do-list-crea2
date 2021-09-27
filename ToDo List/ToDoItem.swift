f9938336b15a92cba4677da3bba23c3347cacfb1
crea2 <reacc@bc.edu>
2021-09-27 08:33:23 -0400

    Finished 5.6

---
+++ ToDo List/ToDoItem.swift
@@ -0,0 +1,14 @@
+//
+//  ToDoItem.swift
+//  ToDo List
+//
+//  Created by Christopher Rea on 9/27/21.
+//
+
+import Foundation
+
+struct ToDoItem {
+    var name: String
+    var date: Date
+    var notes: String
+}
